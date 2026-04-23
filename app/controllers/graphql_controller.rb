17606fa30634a419fd7d5ff1f3a506d927345d07
Chander Prakash <chanderprakash@apples-MacBook-Pro.local>
2026-04-24 00:21:05 +0530

    first commit

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -14,7 +14,7 @@ class GraphqlController < ApplicationController
     context = {
                 current_user: current_user
               }
-    result = RailsTemplateSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
+    result = BakesOnDemandSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue StandardError => e
     raise e unless Rails.env.development?
--- app/graphql/bakes_on_demand_schema.rb
+++ app/graphql/bakes_on_demand_schema.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class RailsTemplateSchema < GraphQL::Schema
+class BakesOnDemandSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
